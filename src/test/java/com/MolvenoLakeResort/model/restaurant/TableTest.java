e00eba64cc6fcd61cad488a33baffdda8cbd7685
smourik <sanne.van.mourik@capgemini.com>
2018-06-13 16:18:18 +0200

    Created test for Table class: TableTest.

---
+++ src/test/java/com/MolvenoLakeResort/model/restaurant/TableTest.java
@@ -0,0 +1,33 @@
+package com.MolvenoLakeResort.model.restaurant;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+public class TableTest {
+    private Table table;
+
+    @Before
+    public void setUp() {
+        this.table = new Table(0, 3);
+    }
+
+    @Test
+    public void testSetAndGetForTableID() {
+
+        this.table.setTableID(5);
+
+        int actual = this.table.getTableID();
+        Assert.assertEquals(5, actual);
+    }
+
+    @Test
+    public void testSetAndGetForCapacity(){
+
+        this.table.setCapacity(6);
+
+        int actual = this.table.getCapacity();
+        Assert.assertEquals(6, actual);
+
+    }
+}
