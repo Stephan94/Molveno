0c026918045f022be431e8444ad40e462dd5f7db
FunkyFeudalFox <arjan_tammer@yahoo.co.uk>
2018-06-14 13:34:59 +0200

    removed ID from constructors of Room and Booking

--- src/main/java/com/MolvenoLakeResort/model/hotel/Booking.java
+++ src/main/java/com/MolvenoLakeResort/model/hotel/Booking.java
@@ -16,5 +16,59 @@ public class Booking {
 
     private int numberOfGuests;
 
+    public int getID() {
+        return ID;
+    }
+
+    public void setID(int ID) {
+        this.ID = ID;
+    }
+
+    public LocalDateTime getCheckInDate() {
+        return checkInDate;
+    }
+
+    public void setCheckInDate(LocalDateTime checkInDate) {
+        this.checkInDate = checkInDate;
+    }
+
+    public LocalDateTime getCheckOutDate() {
+        return checkOutDate;
+    }
+
+    public void setCheckOutDate(LocalDateTime checkOutDate) {
+        this.checkOutDate = checkOutDate;
+    }
+
+    public double getTotalBookingCost() {
+        return totalBookingCost;
+    }
+
+    public void setTotalBookingCost(double totalBookingCost) {
+        this.totalBookingCost = totalBookingCost;
+    }
+
+    public boolean isPaid() {
+        return isPaid;
+    }
+
+    public void setPaid(boolean paid) {
+        isPaid = paid;
+    }
+
+    public int getNumberOfGuests() {
+        return numberOfGuests;
+    }
+
+    public void setNumberOfGuests(int numberOfGuests) {
+        this.numberOfGuests = numberOfGuests;
+    }
+
+    public Booking(LocalDateTime checkInDate, LocalDateTime checkOutDate, int numberOfGuests){
+        this.setCheckInDate(checkInDate);
+        this.setCheckOutDate(checkOutDate);
+        this.setNumberOfGuests(numberOfGuests);
+    }
+
 
 }
