7ae3a723633175343108aa89ba067dd2dda0c19f
mosalem <mo.salem.92@gmail.com>
2018-06-26 14:25:38 +0200

    prepare moving to spring

--- src/main/java/com/MolvenoLakeResort/App.java
+++ src/main/java/com/MolvenoLakeResort/App.java
@@ -3,12 +3,18 @@ package com.MolvenoLakeResort;
 import com.MolvenoLakeResort.model.restaurant.Restaurant;
 import com.MolvenoLakeResort.model.restaurant.Table;
 import com.MolvenoLakeResort.controller.AppController;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-
+@SpringBootApplication
 public class App {
     public static void main(String[] args) {
 
+        /*
         AppController app = new AppController();
         app.run();
+        */
+
+        SpringApplication.run(App.class, args);
     }
 }
