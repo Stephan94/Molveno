208b0c50624f42a94a48711410e160470bcef326
shanaFriedRice <gourmetshana@gmail.com>
2018-06-27 16:27:06 +0200

    Add TableController

---
+++ src/main/java/com/MolvenoLakeResort/model/restaurant/TableController.java
@@ -0,0 +1,42 @@
+import com.MolvenoLakeResort.model.restaurant.Table;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Collection;
+
+/*
+@RestController
+@RequestMapping("api/tables")
+public class TableController {
+
+    @Autowired
+    private TableRepository tableRepository;
+
+    @PostMapping
+    public Table create(@RequestBody Table newTable){
+        this.tableRepository.save(newTable);
+        return newTable;
+    }
+
+    @GetMapping
+    public Collection<Table> list(){
+        return this.tableRepository.findAll();
+    }
+
+    @GetMapping("{id}")
+    public Table findById(@PathVariable long id){
+        Table result = this.tableRepository.findById(id);
+        return result;
+    }
+
+    @PutMapping("{id}")
+    public Table updateById(@PathVariable long id, @RequestBody Table update){
+        return this.tableRepository.update(id, update);
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public void deleteById(@PathVariable long id){
+        this.tableRepository.deleteById(id);
+    }
+}
+*/
